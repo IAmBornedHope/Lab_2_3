d317b7d09197e8cb4d8f0d6b0a4483584aaef960
IAmBornedHope <realbeloff@gmail.com>
2026-05-08 00:06:44 +0300

    Complex operators + Complex format

--- my_types/Complex.hpp
+++ my_types/Complex.hpp
@@ -1,8 +1,13 @@
 #pragma once
+#include <iostream>
 #include <format>
+#include <concepts>
 #include "../exceptions/Exceptions.hpp"
 
 template<typename T>
+concept Arithmetic = std::is_arithmetic_v<T>;
+
+template<Arithmetic T>
 class Complex {
 private:
     T re_;
@@ -37,4 +42,49 @@ public:
         return Complex(re, im);
     }
 
+    Complex operator/(const Complex& other) const {
+        T denominator = (other.re_ * other.re_) + (other.im_ * other.im_);
+        if (denominator == 0) {
+            throw DivisionByZeroException("Complex. Деление на ноль.");
+        }
+        T re = ((re_ * other.re_) + (im_ * other.im_)) / denominator;
+        T im = ((im_ * other.re_) - (re_ * other.im_)) / denominator;
+        
+        return Complex(re, im);
+    }
+
+    bool operator==(const Complex& other) const {
+        return (re_ == other.re_) && (im_ == other.im_);
+    }
+
+    bool operator!=(const Complex& other) const {
+        return !(*this == other);
+    }
+
+    std::string complex_to_string() const {
+
+        double new_re = static_cast<double>(re_);
+        double new_im = static_cast<double>(im_);
+
+        if (re_ == 0 && im_ == 0) {
+            return "0";
+        }
+        if (im_ == 0) {
+            return std::format("{:.2f}", new_re);
+        }
+        if (re_ == 0) {
+            return std::format("{:.2f}i", new_im);
+        }
+        if (im_ > 0) {
+            return std::format("{:.2f}+{:.2f}i", new_re, new_im);
+        }
+        return std::format("{:.2f}{:.2f}i", new_re, new_im);
+    }
+
 };
+
+template<Arithmetic T>
+std::ostream& operator<<(std::ostream& stream, const Complex<T>& complex) {
+    stream << complex.complex_to_string();
+    return stream;
+}
